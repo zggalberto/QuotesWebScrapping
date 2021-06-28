0aeec09ac101c7056bc3fbce257785c280fa646b
Iknowthings <Iknowthings0001@gmail.com>
2021-06-27 21:38:07 -0500

    Adding comments

--- Quotes.py
+++ Quotes.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-
+# Get page
 URL='https://www.frasess.net/frases-de-la-vida-cortas-para-reflexionar-1.html'
 page = requests.get(URL)
 
+#Parse the page
 soup = BeautifulSoup(page.content, 'html.parser')
 
+#Find all div elements with class="quote_text"
 quotes = soup.find_all("div",class_="quote_text")
 
-
+#for each element extract text and auhtor"
 for quote in quotes:
     message=quote.find(text=True,recursive=False)
     author=quote.find("div",class_="quote_author")
